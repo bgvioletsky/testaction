6231a4ed9cfae466db85c9c393852f721c0931a6
bgcode <violetcode@icloud.com>
2024-01-21 23:48:28 +0800

    bgcode

--- download_image.py
+++ download_image.py
@@ -8,18 +8,18 @@ def download_image(url, file_path):
         f.write(response.content)  # 将响应内容写入文件
 
 def main():
-    # now = datetime.now()  # 获取当前日期时间
-    # date = now.date()  # 获取当前日期
-    # folder = str(date)  # 文件夹名称为日期字符串
-    # if not os.path.exists(folder):  # 如果文件夹不存在，则创建文件夹
-    #     os.makedirs(folder)
+    now = datetime.now()  # 获取当前日期时间
+    date = now.date()  # 获取当前日期
+    folder = str(date)  # 文件夹名称为日期字符串
+    if not os.path.exists(folder):  # 如果文件夹不存在，则创建文件夹
+        os.makedirs(folder)
         
     url = 'https://www.dmoe.cc/random.php?return=json'
     turl = requests.get(url).json()
     url = turl['imgurl']
     filename = url.split("/")[-1]
-    # file_path = os.path.join(folder, filename)  # 构建完整的文件路径
-    file_path='./'+filename
+    file_path = os.path.join(folder, filename)  # 构建完整的文件路径
+    # file_path='./'+filename
     download_image(url, file_path)
 
 if __name__ == '__main__':
